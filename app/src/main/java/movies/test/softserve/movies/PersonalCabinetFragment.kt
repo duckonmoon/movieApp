ff5123b8624a3a0bbc3bc1b31c21a2d62c5ee8f4
duckonmoon <rostyk1997@gmail.com>
2018-01-09 10:34:47 +0200

    added photo c ashing

--- app/src/main/java/movies/test/softserve/movies/PersonalCabinetFragment.kt
+++ app/src/main/java/movies/test/softserve/movies/PersonalCabinetFragment.kt
@@ -6,24 +6,38 @@ import android.content.Intent
 import android.graphics.Bitmap
 import android.os.Bundle
 import android.support.v4.app.Fragment
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import com.google.firebase.storage.FirebaseStorage
 import com.squareup.picasso.Callback
 import com.squareup.picasso.Picasso
 import kotlinx.android.synthetic.main.fragment_personal_cabinet.view.*
 import movies.test.softserve.movies.controller.MainController
 import java.io.ByteArrayOutputStream
+import java.io.File
+import java.io.FileOutputStream
+import java.io.IOException
 
 
 class PersonalCabinetFragment : Fragment() {
 
     private lateinit var thisView: View
-    private val storage = FirebaseStorage.getInstance().getReference("user").child(MainController.getInstance().user.uid).child("user_Photo")
+    private val user = MainController.getInstance().user
+    private val storage = FirebaseStorage.getInstance().getReference("user").child(user.uid).child("user_Photo")
+
+    private lateinit var fileWithLastPhoto: File
 
     companion object {
+        val NAME_OF_FILE = "img"
         val SELECT_PICTURE = 1
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        fileWithLastPhoto = File(context!!.filesDir, NAME_OF_FILE)
     }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
@@ -34,9 +48,34 @@ class PersonalCabinetFragment : Fragment() {
             pickImage()
         }
 
+        thisView.nickname.setText(user.displayName)
+
+        try {
+            if (fileWithLastPhoto.exists()) {
+                Picasso.with(activity).load(fileWithLastPhoto).into(thisView.profile_photo)
+            }
+        } catch (e: Exception) {
+            Log.e("Smth wrong with image", e.message)
+        }
+
         storage.downloadUrl.addOnSuccessListener { uri ->
+            Picasso.with(activity).cancelRequest(thisView.profile_photo)
             Picasso.with(activity).load(uri).noPlaceholder().centerCrop().fit()
-                    .into(thisView.profile_photo)
+                    .into(thisView.profile_photo,object :Callback{
+                        override fun onSuccess() {
+                            tacticalSleeping()
+
+                            val bitmap = buildBitmapFromImage(thisView.profile_photo)
+
+                            saveToFile(bitmap)
+                        }
+
+                        override fun onError() {
+                            TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+                        }
+
+                    })
+
         }
 
         return thisView
@@ -54,6 +93,7 @@ class PersonalCabinetFragment : Fragment() {
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
         if (resultCode == RESULT_OK && requestCode == SELECT_PICTURE) {
+            Picasso.with(activity).cancelRequest(thisView.profile_photo)
             Picasso.with(activity).load(data!!.data).noPlaceholder().centerCrop().fit()
                     .into(thisView.profile_photo, object : Callback {
                         override fun onError() {
@@ -61,15 +101,12 @@ class PersonalCabinetFragment : Fragment() {
                         }
 
                         override fun onSuccess() {
-                            Thread.sleep(200)
-                            thisView.profile_photo.isDrawingCacheEnabled = true
-                            thisView.profile_photo.buildDrawingCache()
-                            val bitmap = thisView.profile_photo.drawingCache
-                            val baos = ByteArrayOutputStream()
-                            bitmap.compress(Bitmap.CompressFormat.JPEG, 100, baos)
-                            val data = baos.toByteArray()
+                            tacticalSleeping()
+                            val bitmap = buildBitmapFromImage(thisView.profile_photo)
 
-                            storage.putBytes(data)
+                            saveToFile(bitmap)
+
+                            saveToFirebase(bitmap)
                         }
                     })
         }
@@ -79,5 +116,40 @@ class PersonalCabinetFragment : Fragment() {
     override fun onDestroy() {
         Picasso.with(activity).cancelRequest(thisView.profile_photo)
         super.onDestroy()
+    }
+
+    private fun tacticalSleeping(){
+        Thread.sleep(200)
+    }
+
+    private fun buildBitmapFromImage(image : ImageView) : Bitmap {
+        image.isDrawingCacheEnabled = true
+        image.buildDrawingCache()
+        return image.drawingCache
+    }
+
+    private fun saveToFile(bitmap: Bitmap){
+        var out: FileOutputStream? = null
+        try {
+            out = FileOutputStream(fileWithLastPhoto)
+            bitmap.compress(Bitmap.CompressFormat.PNG, 100, out)
+        } catch (e: Exception) {
+            e.printStackTrace()
+        } finally {
+            try {
+                if (out != null) {
+                    out.close()
+                }
+            } catch (e: IOException) {
+                e.printStackTrace()
+            }
+        }
+    }
+
+    private fun saveToFirebase(bitmap: Bitmap){
+        val baos = ByteArrayOutputStream()
+        bitmap.compress(Bitmap.CompressFormat.JPEG, 100, baos)
+        val data = baos.toByteArray()
+        storage.putBytes(data)
     }
 }
